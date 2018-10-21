4c1872f555d4410dda78e4de0f83eb63709f7eb5
Brofrong <dima737.99@mail.ru>
2018-10-21 17:58:42 +0300

    new file system

---
+++ core/lib/math.cpp
@@ -0,0 +1,9 @@
+#include "math.h"
+
+
+int abs(int a){
+	if (a>0){
+		return a;
+	}
+	return -a;
+}
--- core/main.cpp
+++ core/main.cpp
@@ -1,26 +1,25 @@
 
-#include "video.cpp"
-#include "keyboard.cpp"
-#include "CMOS.h"
-#include "window.cpp"
-#include "stdio.cpp"
-//#include "pong.cpp"
-#include "disc.cpp"
-#include "imgView.cpp"
+#include "system/video.cpp"
+#include "system/keyboard.cpp"
+#include "system/CMOS.h"
+#include "lib/window.cpp"
+#include "lib/stdio.cpp"
+//#include "program/pong.cpp"
+#include "system/disc.cpp"
+#include "program/imgView.cpp"
 
 int _start(){
 	hdd disk = hdd();
 	disk.readData(0x80000,2,0x18);
 	out io = out();
-	io.multiply=5;
-	io.screen.drawLine(400,400,600,400);
-	io.printChar('a');
-	io.printString("ab\nQWERTYUIOPASDFGHJKLZXCVBNM\n1234567890-=`'/");
+	io.multiply=1;
+
+	io.printString("qwertyu12334567ab\nQWERTYUIOPASDFGHJKLZXCVBNM\n1234567890-=`'/");
 	//video screen = video();
 	window win = window();
 	//pong = pongGame();
 	//keyboard key = keyboard();
-	//imgView prog = imgView();
+	imgView prog = imgView();
 	//io.multiply=5;
 
 	
@@ -29,7 +28,7 @@ int _start(){
 	while(1){
 		if (win.FPSLimiter(60)){
 			win.draw();
-			//prog.update();
+			prog.update();
 			//pong.draw();
 			//io.drawPointer();
 			io.input();
